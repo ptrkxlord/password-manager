1ad4641e3389c63cf1969c8d3f0712c8b1e46694
ptrkxlord <ptrkxlord@users.noreply.github.com>
2026-03-26 15:58:00 +0200

    feat(security): реалізація KeyDerivationService за допомогою Argon2id

---
+++ client/src/security/KeyDerivationService.cpp
@@ -0,0 +1,34 @@
+#include "KeyDerivationService.h"
+#include <libsodium.h>
+
+KeyDerivationService::KeyDerivationService(QObject *parent) : QObject(parent) {}
+
+QByteArray KeyDerivationService::deriveKey(const QString &password, const QByteArray &salt, 
+                                          unsigned long long opslimit, size_t memlimit, 
+                                          size_t keyLen)
+{
+    QByteArray key;
+    key.resize(keyLen);
+    
+    QByteArray passBytes = password.toUtf8();
+
+    if (crypto_pwhash_argon2id(
+            (unsigned char *)key.data(), key.size(),
+            passBytes.constData(), passBytes.size(),
+            (const unsigned char *)salt.constData(),
+            opslimit, memlimit,
+            crypto_pwhash_ALG_ARGON2ID13
+        ) != 0) {
+        return QByteArray();
+    }
+
+    return key;
+}
+
+QByteArray KeyDerivationService::generateSalt()
+{
+    QByteArray salt;
+    salt.resize(crypto_pwhash_SALTBYTES);
+    randombytes_buf(salt.data(), salt.size());
+    return salt;
+}

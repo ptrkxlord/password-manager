c7186de95528a4ee1efc05c241209736d63018b0
ptrkxlord <ptrkxlord@users.noreply.github.com>
2026-03-26 15:58:41 +0200

    feat(client): реєстрація VaultManager у QML та оновлення CMakeLists.txt

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,6 +1,8 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include "domain/VaultManager.h"
+#include "domain/EntryService.h"
 
 int main(int argc, char *argv[])
 {
@@ -10,7 +12,16 @@ int main(int argc, char *argv[])
     app.setOrganizationDomain("ptkxlord.com");
     app.setApplicationName("ptkxlord");
 
+    VaultManager vaultManager;
+    EntryService entryService(nullptr); // Initialized with nullptr, will be set when vault opens? 
+    // Wait, EntryService needs a db manager. Better to pass the db manager from VaultManager.
+
     QQmlApplicationEngine engine;
+    
+    qmlRegisterType<VaultManager>("com.ptkxlord.security", 1, 0, "VaultManager");
+    // Or just set context properties
+    engine.rootContext()->setContextProperty("vaultManager", &vaultManager);
+
     const QUrl url(u"qrc:/qml/main.qml"_qs);
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
